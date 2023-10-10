4e75e3af7d8428af9a0eccf73763097349b40ab2
Lorenzo <57161796+lpolverino@users.noreply.github.com>
2023-10-10 19:07:02 -0300

    Delete src directory

--- src/components/Navbar/NavbarLinks/NavbarLinks.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const NavbarLinks = ({href, label, active, onClick}) => {
-  return (
-    <li><a href={href} className={active ? 'active':''} onClick={onClick}>{label}</a></li>
-  )
-}
-
-export default NavbarLinks
